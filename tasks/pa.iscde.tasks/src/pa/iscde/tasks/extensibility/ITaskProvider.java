2e6f1647a96344bda5c2f4f86042a0acc786f31b
ngmachado <ngsmo@iscte-iul.pt>
2016-01-12 15:06:24 +0000

    Added comments

--- tasks/pa.iscde.tasks/src/pa/iscde/tasks/extensibility/ITaskProvider.java
+++ tasks/pa.iscde.tasks/src/pa/iscde/tasks/extensibility/ITaskProvider.java
@@ -4,10 +4,25 @@ import java.util.List;
 
 public interface ITaskProvider {
 	
+	/**
+	 * Return the Provider Name
+	 * 
+	 * @return String
+	 */
 	public String getProviderName();
 	
+	/**
+	 * Get a list of task from the Provider
+	 * 
+	 * @return List<ITask>
+	 */
 	public List<ITask> getTasks(); 
 	
+	/**
+	 * Define action to be perform in ITask task
+	 * 
+	 * @param task ITask that action will be perform
+	 */
 	public void performAction(ITask task);
 	
 }
